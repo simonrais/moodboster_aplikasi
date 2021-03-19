ddf4bc607551941e2de8a503b931a24b6515db76
= <=>
2021-03-19 10:52:28 +0700

    Moodbooster First Commit

---
+++ android/app/src/main/kotlin/com/example/moodboster_aplikasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moodboster_aplikasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Moodbooster',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Mood'),
+        ),
+      ),
+    );
+  }
+}
